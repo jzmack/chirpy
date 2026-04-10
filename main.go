c5e11e870a242d531a5dfe0623b43bc3b82e85f3
Jake <canesfanjake1@gmail.com>
2026-04-10 16:49:13 -0400

    added custom http handler

--- main.go
+++ main.go
@@ -5,12 +5,20 @@ import (
 	"net/http"
 )
 
+func myHandler(writer http.ResponseWriter, req *http.Request) {
+	writer.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	writer.WriteHeader(http.StatusOK)
+	writer.Write([]byte(http.StatusText(http.StatusOK)))
+}
+
 func main() {
 	const filePathRoot = "."
 	const port = "8080"
 
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(filePathRoot)))
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filePathRoot))))
+	mux.HandleFunc("/healthz", myHandler)
+
 	srv := &http.Server{
 		Addr:    ":" + port,
 		Handler: mux,
